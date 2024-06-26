d6acf8d3ecf1a4e9041e26e2f014a0815ad9ebc0
Akbar E K <akbar72987@gmail.com>
2024-06-26 17:30:00 +0530

    frontend listing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import Joi from 'joi';
+import cors from 'cors';
 dotenv.config();
 import { Book, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const bookSchema = Joi.object({
